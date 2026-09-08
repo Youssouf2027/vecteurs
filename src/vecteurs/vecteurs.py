0db536cf5e715085869aad1860a77fc3ea7509fd
MAC <mac@MACs-MacBook-Pro.local>
2026-09-08 10:10:16 +0530

    ajout __len__, __getitem__, __iter__, __contains__

--- src/vecteurs/vecteurs.py
+++ src/vecteurs/vecteurs.py
@@ -1,11 +1,10 @@
 class Vecteur:
-    def __init__(self, composantes):
+    def __init__(self,composantes):
         if not composantes:
             raise ValueError("LA liste des composantes ne peut pas être vide")
         if not all(isinstance(x,(int,float)) for x in composantes):
             raise ValueError("Toutes les composantes doivent être des nombres")
         self._composantes = list(composantes) 
-
 
     def __repr__(self):
         return f"Vecteur({self._composantes})"
@@ -14,3 +13,14 @@ class Vecteur:
         if not isinstance(other,Vecteur):
             return NotImplemented
         return self._composantes == other._composantes
+    def __len__(self):
+        return len(self._composantes)
+    def __getitem__(self,index):
+        if isinstance(index,slice):
+            return(Vecteur(self._composantes[index]))
+        return self._composantes[index]
+    def __iter__(self):
+        return iter(self._composantes)
+
+    def __contains__(self,item):
+        return item in self._composantes
--- tests/test_vecteurs.py
+++ tests/test_vecteurs.py
@@ -18,4 +18,22 @@ def test_is_contre_egal():
     v2 = Vecteur([1, 2])
     assert v1 is not v2
     assert v1 == v2
-    
+def test_longueur():
+    v=Vecteur([1,2,3])
+    assert len(v) == 3
+
+def test_get_item():
+    v = Vecteur([1,2,3])
+    assert v[0] == 1
+    assert v[1] == 2
+    assert v[2] == 3
+    assert v[0:2] == Vecteur([1, 2])
+
+def test_iter():
+    v = Vecteur([1,2,3])
+    for x in v:
+        assert list(v) == [1, 2, 3]
+def test_contains():
+    v = Vecteur([1,2,3])
+    assert 2 in v
+    assert 4 not in v
